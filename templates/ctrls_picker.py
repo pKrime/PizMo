1e6ea89800e39936551814f77939572ed7934920
pKrime <palucam@gmail.com>
2021-01-11 18:53:00 +0100

    .

--- templates/ctrls_picker.py
+++ templates/ctrls_picker.py
@@ -4,27 +4,35 @@ import PizMo
 from PizMo import storage, shapes, enum_types
 from PizMo.storage import Stock
 from PizMo.shapes import Quad2D
-from PizMo.shapes import MeshShape2D
+from PizMo.shapes import MeshShape3D
 from PizMo.enum_types import ShapeType, WidgetType
 
 
 def create_picker(bone_widgets):
     store = storage.Storage()
-    
-    for bone_name, wdg_mesh_name in bone_widgets.items():
-        wdg = Stock(widget_type=WidgetType.BONE,
-                    shape_type=ShapeType.VERTICES,
-                    position=(0.0, 0.0),
-                    data=dict(bone_name=bone_name,
-                              vertices=MeshShape2D(bpy.data.meshes[wdg_mesh_name],
-                                                   scale=2.0).vertices,))
 
-        store.add_widget(wdg)
+    # for bone_name, wdg_mesh_name in bone_widgets.items():
+    #    print('bone:', bone_name, 'mesh', wdg_mesh_name)
+    #    wdg = Stock(widget_type=WidgetType.BONE,
+    #                shape_type=ShapeType.MESH3D,
+    #                position=(0.0, 0.0),
+    #                data=dict(bone_name=bone_name,
+    #                          object=bpy.data.objects[wdg_mesh_name]))
+
+    #    store.add_widget(wdg)
+
+    wdg = Stock(widget_type=WidgetType.BONE,
+                shape_type=ShapeType.MESH3D,
+                position=(0.0, 0.0),
+                data=dict(bone_name='ik-bone_33.L',
+                          object=bpy.data.objects['GEO-all'],
+                          vertex_group='bone_33.L'))
+    store.add_widget(wdg)
 
 
 if __name__ == "__main__":
     bone_widgets = {
-        'ik-bone_33.L': 'WDG-foot.L',
+        # 'ik-bone_33.L': 'WDG-foot.L',
         'ik-poleleg.001.L': 'WDG-leg.L',
         'ik-bone_33.R': 'WDG-foot.R',
         'ik-poleleg.001.R': 'WDG-leg.R',
